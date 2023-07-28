25522ac4bf85b3b123c66651af7496f0eb8b72bd
A.R.L <luminyouyazj@126.com>
2023-07-28 22:47:38 +0800

    finish user page basic layout

--- app/src/main/java/com/example/mybaidu/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/mybaidu/ui/notifications/NotificationsFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.mybaidu.R
 import com.example.mybaidu.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
@@ -27,11 +31,10 @@ class NotificationsFragment : Fragment() {
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        var imageView:ImageView = root.findViewById(R.id.user_avatar)
+        val option:RequestOptions = RequestOptions().circleCrop()
+        Glide.with(imageView.context).load("https://images.unsplash.com/photo-1602459599126-6cb8d0d80193?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80").
+        apply(option).into(imageView)
         return root
     }
 
